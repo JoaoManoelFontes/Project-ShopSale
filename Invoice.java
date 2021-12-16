48a9496b9d258b5c2a883778f7f3d752e0f5448c
Manu <84148859+JoaoManoelFontes@users.noreply.github.com>
2021-12-16 10:38:41 -0300

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -2,19 +2,26 @@ package shopsale;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 public class Invoice {
+    
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-  public void invoiceSale(String product, double price, double quantity,double amount){
+    
+  public void invoiceSale(String product, double price, double quantity,double amount,double change, String payment){
+      
       System.out.println("---------------------------------------");
       System.out.println("      CUPOM FISCAL                     ");
       System.out.println("                                       ");
       System.out.println(dtf.format(LocalDateTime.now())+"                  ");
       System.out.println("--------------------------------------");
       System.out.println("PRODUTO: "+product+"              "+"PREÇO:"+price);
-      System.out.println("QUANTIDADE: "+ quantity);
+      System.out.println("QUANTIDADE: "+ quantity+"         "+"TROCO:"+change);   
       System.out.println("--------------------------------------");
+      System.out.println("");
+      System.out.println("PAGAMENTO: "+payment);
       System.out.println("");
       System.out.println("TOTAL: "+amount);
       System.out.println("");
       System.out.println("--------------------------------------");
+      
   } 
+  
 }
