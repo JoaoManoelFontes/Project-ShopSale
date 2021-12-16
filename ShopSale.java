478241085876696b3497985693dadb47b35104fc
Manu <84148859+JoaoManoelFontes@users.noreply.github.com>
2021-12-16 10:36:07 -0300

    Update ShopSale.java

--- ShopSale.java
+++ ShopSale.java
@@ -6,30 +6,34 @@ public class ShopSale {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Payment payment = new Payment();
+         
         System.out.println("Bem vindo ao ShopSale!");
         System.out.println("----------------------");
         System.out.println("");
+        
         System.out.println("Qual é o produto?");
-        String product = sc.next();
+        String product = sc.next();        
+        
         System.out.println("Qual o preço?");
         String price = sc.next();
-        price = price.replace(",", ".");
+        price = price.replace(",", ".");        
         double priceDouble = Double.parseDouble(price);
+        
         System.out.println("Qual a quantidade?");
         double quantity = sc.nextDouble();
         double amount = (quantity*priceDouble);
-        System.out.println("Total da venda:");
-        System.out.println(amount);
-        payment.setAmount(amount);
-        payment.setPrice(priceDouble);
-        payment.setQuantity(quantity);
-        payment.setProduct(product);
-        System.out.println("Deseja fechar a venda?(apenas sim ou nao)");
+        
+        System.out.println("Total: "+amount);
+        
+        Payment payment = new Payment(amount, priceDouble,quantity,product);
+                        
+        System.out.println("Deseja fechar a venda?(sim para fechar) ");
         String finishSale = sc.next();
+        
         System.out.println("");
         System.out.println("------------------------------------------");
         System.out.println("");
+        
         if (finishSale.equals("sim")){
             System.out.println("Opções de pagamento:");
             System.out.println("digite 1 se for no dinheiro");
@@ -40,12 +44,12 @@ public class ShopSale {
             System.out.println("");
             System.out.println("---------------------------------------");
             System.out.println("");
-        } else if (!finishSale.equals("nao")) {
-            System.out.println("Opção inválida!Digite apenas sim ou nao");
-
+            
         } else {
             System.out.println("Venda cancelada!");
         }
+        
+        sc.close();
     }
 
 }
